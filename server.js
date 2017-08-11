2bac699e78dec02c8c751d158c938fce12fe5109
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-11 18:07:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,10 +105,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
-   });
 
 var names = [];
 app.get('/submit-name', function(req, res){
@@ -116,6 +112,12 @@ app.get('/submit-name', function(req, res){
     names.push('name');
     res.send(JSON.stringify(names));
 });
+
+app.get('/:articleName', function (req, res){
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
+   });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
