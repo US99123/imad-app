14b69e71cc6e36dff252a5bf68acb06c71a19141
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-11 19:02:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('counter');
+/*var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function() {
@@ -20,7 +20,7 @@ span.innerHTML = counter.toString();
 };
 
 //make a request
-request.open('GET','http://erutsavsolanki27.imad.hasura-app.io/counter',true);
+request.open('GET','http://girijaiyer1996.imad.hasura-app.io/counter',true);
 request.send(null);
 
 };
@@ -57,12 +57,8 @@ var nameInput = document.getElementById('n');
 var n = nameInput.value;
 request.open('GET','http://erutsavsolanki27.imad.hasura-app.io/submit-name?name='+ n,true);
 request.send(null);
-};
+};*/
 
-
-
-
-/*
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
@@ -100,4 +96,4 @@ submit.onClick = function(){
  ul.innerHTML = list;
  
  
-};*/
+};
