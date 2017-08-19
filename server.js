293107574f71e54590cfcdedd76c7b500478c9a6
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-19 17:33:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ return htmlTemplate;
 }
 
 var counter = 0;
-app.get('/counter', function (req, res) {
+//app.get('/counter', function (req, res) {
 counter = counter + 1;
 res.send(counter.toString());
 });
