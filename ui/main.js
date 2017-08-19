e124d7fc7d4893d9170a240316dfa38da03908e5
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-19 11:45:23 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,31 @@
+
+ <div id='main text' class="center text-big bold">
+                           G I R I Z A 
+            </div>
+    	    <div class="center text-big bold">
+    	      !!  My First Web App !!
+    	    </div>
+    	    <br>
+
+    	    <hr>
+            <h3> Quick Bites</h3>
+            <div>
+                1. This is a recreational webapp
+                2. Every page of this webapp was a learning step for me
+                3. It's my first webapp ever! ;)
+            </div>
+
+            <br>
+            <div class = "footer"> 
+            This button <button id="counter"> Click Me</button> has been clicked <span id="count"> 0 </span>times.
+            </div>
+            <hr>
+            <br>
+            <input type="text" placeholder="Name"  id="n" ></input>
+            <input type="submit" value="Submit" id="submit_btn"></input>
+            <ul id="namelist">
+
+            </ul>
+   </div>
+   <script type = "text/javascript" src="/ui/main.js"> </script>
+</body>
