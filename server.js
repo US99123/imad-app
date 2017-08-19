63cb63b8943f8624cb21120ec9f4cd9c49ef2ff1
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-19 22:59:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-
-/*var express = require('express');
+var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-/*var pool = require('pg').pool;
+
 var app = express();
 app.use(morgan('combined'));
-var config = {
-    user:'erutsavsolanki27',
-    database:'erutsavsolanki27',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password: process.env.DB_PASSWORD
-    
-};
 
 var articles = {
     
@@ -100,43 +91,8 @@ return htmlTemplate;
 }
 
 
-var counter = 0;
-//app.get('/counter', function (req, res) {
-counter = counter + 1;
-//res.send(counter.toString());
-//});
-/*var counter = 0;
-app.get('/counter', function(req, res){
-    counter = counter +1;
-    res.send(counter.toString());
-});*/
-
-
-//app.get('/', function (req, res) {
-  //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-//});
-
-//var pool = new pool(config);
-/*app.get('/test-db', function(req, res){
-    
-    pool.query('SELECT * FROM test', function(err, result) {
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            res.send(JSON.stringify(result));
-        }
-    });*/
-    
-});
-
-
-
-//var names = [];
-
-/*app.get('/submit-name', function(req, res){
-    var name = req.query.name;
-    names.push('name');
-    res.send(JSON.stringify(names));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName', function (req, res){
@@ -160,8 +116,4 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-
-;
-
-
-*/
+});
