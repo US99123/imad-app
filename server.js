6c39216a052f48139038438d2a03064097ac1900
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-19 17:38:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,8 @@ app.get('/counter', function(req, res){
 
 
 
-var names = [];/*
+//var names = [];
+/*
 app.get('/submit-name', function(req, res){
     var name = req.query.name;
     names.push('name');
