cea04748829f059c0bd04e957d030c7204af4f20
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-19 17:36:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/counter', function(req, res){
   //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 //});
 
-var pool = new pool(config);
+//var pool = new pool(config);
 app.get('/test-db', function(req, res){
     
     pool.query('SELECT * FROM test', function(err, result) {
