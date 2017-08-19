6a10a9ecf6a9d4e8c807c2f76c6801d6e85848ff
US99123 <30657412+US99123@users.noreply.github.com>
2017-08-19 23:46:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var Pool = require('pg').Pool;
-var pool = new Pool(config);
+
 var config = {
     user:'erutsavsolanki27',
     database:'erutsavsolanki27',
@@ -104,7 +104,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var pool = new Pool(config);
 app.get('/test-db', function (req, res){
     pool.query('SELECT * FROM test', function(err, result) {
         if(err){
